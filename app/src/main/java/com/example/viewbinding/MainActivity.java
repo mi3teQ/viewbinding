8a524ae43660b6ebe62eb4bf449934b85b03403b
mietek <nowakstas6@gmail.com>
2021-11-15 15:59:09 +0100

    kodu

--- app/src/main/java/com/example/viewbinding/MainActivity.java
+++ app/src/main/java/com/example/viewbinding/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+
 import com.example.viewbinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
 
     ActivityMainBinding activityMainBinding;
+    Switch WylaczNick, WylaczImie, WylaczNazwe, WylaczWiek, WylaczNumer, WylaczEmail;
+    EditText PodajNick, PodajImie, PodajNazwe, PodajWiek, PodajNumer, PodajEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = activityMainBinding.getRoot();
         setContentView(view);
+
+        WylaczNick = (Switch) findViewById(R.id.WylaczNick);
+        WylaczImie = (Switch) findViewById(R.id.WylaczImie);
+        WylaczNazwe = (Switch) findViewById(R.id.WylaczNazwe);
+        WylaczWiek = (Switch) findViewById(R.id.WylaczWiek);
+        WylaczNumer = (Switch) findViewById(R.id.WylaczNumer);
+        WylaczEmail = (Switch) findViewById(R.id.WylaczEmail);
+
+        PodajNick = (EditText) findViewById(R.id.PodajNick);
+        PodajImie = (EditText) findViewById(R.id.PodajImie);
+        PodajNazwe = (EditText) findViewById(R.id.PodajNazwe);
+        PodajWiek = (EditText) findViewById(R.id.PodajWiek);
+        PodajNumer = (EditText) findViewById(R.id.PodajNumer);
+        PodajEmail = (EditText) findViewById(R.id.PodajEmail);
+
+
+        WylaczNick.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    PodajNick.setEnabled(false);
+                }else{
+                    PodajNick.setEnabled(true);
+                }
+            }
+        });
 
     }
 }
